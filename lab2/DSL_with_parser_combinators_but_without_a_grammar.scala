baa3e3224b83d6b34a0a6a8e029eec2e15ce2eb7
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-12-24 13:08:09 +0100

    End DSL_with_parser_combinators_but_without_a_grammar
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lab2/DSL_with_parser_combinators_but_without_a_grammar.scala
+++ lab2/DSL_with_parser_combinators_but_without_a_grammar.scala
@@ -1,4 +1,5 @@
 // scalac -classpath "./scala-parser-combinators_2.13-2.3.0.jar" DSL_with_parser_combinators_but_without_a_grammar.scala && scalac -classpath "./scala-parser-combinators_2.13-2.3.0.jar" test.scala
+// scala -cp .:./scala-parser-combinators_2.13-2.3.0.jar DSL_with_parser_combinators_but_without_a_grammar.scala
 import scala.util.parsing.combinator._
 
 class TestParser extends JavaTokenParsers {
@@ -15,13 +16,21 @@ class TestParser extends JavaTokenParsers {
   var variables = Map[String, Int]()
 
   // def expr: Parser[Any] = fact ~ opt(op ~ fact)
-  def assign = (id ~ "=" ~ expr) ^^ { case v ~ "=" ~ e => variables += (v -> e); e }
+  def start = assign | expr
+
+  def assign: Parser[Int] = (id ~ "=" ~ assign) ^^ { case v ~ "=" ~ e => variables += (v -> e); e } | expr
   def id = """[a-zA-Z_]\w*""".r
-  def expr = (fact ~ op ~ fact) ^^ { case t1 ~ opf ~ t2 => opf.get(t1, t2) } | fact
-  def op = ("*" | "/") ^^ { map1.get(_) }
-  def fact = (term ~ op2 ~ term) ^^ { case t1 ~ opf ~ t2 => opf.get(t1, t2) } | term
-  def op2 = ("+" | "-") ^^ { map1.get(_) }
-  def term: Parser[Int] = ("(" ~> expr <~ ")") ^^ { _.toInt } | decimalNumber ^^ { _.toInt } | fun ^^ { _.toInt }
+
+  def expr: Parser[Int] = (fact ~ op ~ expr) ^^ { case t1 ~ opf ~ t2 => opf.get(t1, t2) } | fact
+  def op = ("+" | "-") ^^ { map1.get(_) }
+
+  def fact: Parser[Int] = (term ~ op2 ~ fact) ^^ { case t1 ~ opf ~ t2 => opf.get(t1, t2) } | term
+  def op2 = ("*" | "/") ^^ { map1.get(_) }
+
+  def term: Parser[Int] = (("(" ~> expr <~ ")") ^^ { _.toInt }
+                           | decimalNumber ^^ { _.toInt }
+                           | "-" ~> decimalNumber ^^ { -1 * _.toInt }
+                           | fun ^^ { _.toInt })
   def fun = {
     ("sqrt" ~> "(" ~> expr <~ ")") ^^ { map2.get("sqrt").get(_) } |
     ("sin" ~> "(" ~> expr <~ ")") ^^ { map2.get("sin").get(_) } |
@@ -34,10 +43,19 @@ object TestMain {
   def main(args: Array[String]) = {
     // val lines = scala.io.Source.fromFile(args(0)).mkString
     val parser = new TestParser
+    val r = parser.parseAll(parser.expr, "1 * 1 * 1")
+    println(r)
+    val r2 = parser.parseAll(parser.expr, "1 + 1 + 1")
+    println(r2)
     val result = parser.parseAll(parser.expr, "sqrt(4) + 100 * 2")
     println(result)
     val result2 = parser.parseAll(parser.assign, "x = 100")
-    println(parser.variables)
+    // println(parser.variables)
+    val result3 = parser.parseAll(parser.start, "x = y = 1 + 1")
+    println(result3)
+    // println(parser.variables)
+    val result4 = parser.parseAll(parser.start, "-1 * (1 + sqrt(4) + 100 * 2)")
+    println(result4)
   }
 }
 
