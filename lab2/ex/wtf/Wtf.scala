63737124485980b9356617c36c82c28c79e27d59
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-01-02 17:16:08 +0100

    Start implementing `wtf`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

---
+++ lab2/ex/wtf/Wtf.scala
@@ -0,0 +1,42 @@
+// scalac -cp .:./../../scala-parser-combinators_2.13-2.3.0.jar ./Wtf.scala && scala -cp .:./../../scala-parser-combinators_2.13-2.3.0.jar WtFEvaluator ops.wtf
+
+import scala.util.parsing.combinator._
+
+class WtfParser extends RegexParsers {
+  def program: Parser[Any] = rep(stmt)
+
+  def stmts = rep(stmt)
+  // def funs = rep(fun)
+  //
+  // def fun = "def" ~ fun_name ~ args ~ "=" ~ body_fun
+  def fun_name = """[A-Z]""".r
+  // def args = """[0-9]""".r
+  // def body_fun = ""
+
+  def stmt: Parser[Any] = print | fun_call | expr
+
+  def expr: Parser[Any] = "0" ~ rep(("+" | "-")) | "0"  // | tern_if
+
+  def fun_call = rep(expr) ~ fun_name
+  //
+  // def tern_if = expr ~ "?" ~ if_body ~ ":" ~ if_body
+  // def if_body = "[" ~ stmts ~ "]"
+  //
+  def print = stmt ~ "!"
+}
+
+
+object WtfEvaluator {
+  def main(args: Array[String]): Unit = {
+    val p = new WtfParser
+    args.foreach { arg =>
+      val source = scala.io.Source.fromFile(arg)
+      val lines = try source.mkString finally source.close()
+      p.parseAll(p.program, lines) match {
+        case p.Success(result, _) => println(result)
+        case p.NoSuccess(msg, _) => println(msg)
+      }
+    }
+  }
+
+}
