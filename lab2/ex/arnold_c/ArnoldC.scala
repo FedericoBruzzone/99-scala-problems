6039097653241580d69c8bbd2add72886c9edce0
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-01-02 14:56:07 +0100

    Add ArnoldC
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lab2/ex/arnold_c/ArnoldC.scala
+++ lab2/ex/arnold_c/ArnoldC.scala
@@ -2,35 +2,42 @@
 
 import scala.util.parsing.combinator._
 import scala.io._
+import scala.collection.mutable._
 
 class ArnoldCParser extends JavaTokenParsers {
   var symbol_table = Map[String, Int]()
+  var stack = Stack[Int]()
 
-  def program = "IT'S SHOWTIME" ~> rep(stmt) <~ "YOU HAVE BEEN TERMINATED"
+  def program = "IT'S SHOWTIME" ~> stmts <~ "YOU HAVE BEEN TERMINATED" ^^ { x => x(); symbol_table }
 
-  def stmt = print | init | assgn | if_then_else
+  def stmts: Parser[() => Any] = rep(stmt) ^^ { x => () => x.foreach { stmt => stmt() } }
+  def stmt = print | init | if_then_else | assgn  | loop
+  def print = "TALK TO THE HAND" ~> (id | value | unquotedString) ^^
+    { case id => () => println(id()) }
+  def init= "HEY CHRISTMAS TREE" ~> """[a-zA-Z][a-zA-Z0-9_]*""".r ~ ("YOU SET US UP" ~> value) ^^
+    { case id ~ value => () => symbol_table += (id -> value()) }
+  def if_then_else = "BECAUSE I'M GOING TO SAY PLEASE" ~> id ~ ("[" ~> stmts) ~ ("]" ~> "BULLSHIT" ~> "[" ~> stmts) ~ ("]" ~> "YOU HAVE NO RESPECT FOR LOGIC") ^^
+    { case id ~ if_stmts ~ else_stmts ~ _ => () => if (id() != 0) if_stmts() else else_stmts() }
+  def assgn = "GET TO THE CHOPPER" ~> """[a-zA-Z][a-zA-Z0-9_]*""".r ~ ("HERE IS MY INVITATION" ~> value) ~ exprs <~ "ENOUGH TALK" ^^
+    { case id ~ value ~ exprs => () => { stack.push(value()); exprs(); symbol_table += (id -> stack.pop()) } }
+  def loop = "STICK AROUND" ~> id ~ ("[" ~> stmts) <~ "]" <~ "CHILL" ^^
+    { case id ~ stmts => () => while (id() != 0) stmts() }
 
-  def print = "TALK TO THE HAND" ~> (unquotedString | value)
-  def init = "HEY CHRISTMAS TREE" ~> id ~ ("YOU SET US UP" ~> value)
-  def assgn = "GET TO THE CHOPPER" ~> id ~ ("HERE IS MY INVITATION" ~> value) ~ rep(expr) <~ "ENOUGH TALK"
-  def if_then_else: Parser[Any] = "BECAUSE I'M GOING TO SAY PLEASE" ~> id ~ ("[" ~> rep(stmt)) ~ ("]" ~> "BULLSHIT" ~> "[" ~> rep(stmt)) ~ ("]" ~> "YOU HAVE NO RESPECT FOR LOGIC")
-
+  def exprs: Parser[() => Any] = rep(expr) ^^ { x => () => x.foreach { expr => expr() } }
   def expr = arith | logic
+  def arith = ( "GET UP" ~> value /* + */ ^^ { x => () => { stack.push(stack.pop() + x()) } }
+              | "GET DOWN" ~> value /* - */ ^^ { x => () => { stack.push(stack.pop() - x()) } }
+              | "YOU'RE FIRED" ~> value /* * */ ^^ { x => () => { stack.push(stack.pop() * x()) } }
+              | "HE HAD TO SPLIT" ~> value /* / */ ^^ { x => () => { stack.push(stack.pop() / x()) } })
+  def logic = ( "YOU ARE NOT YOU YOU ARE ME" ~> value /* == */ ^^ { x => () => { stack.push(if (stack.pop() == x()) 1 else 0) } }
+              | "LET OFF SOME STEAM BENNET" ~> value /* > */ ^^ { x => () => { stack.push(if (stack.pop() > x()) 1 else 0) } }
+              | "CONSIDER THAT A DIVORCE" ~> value /* V */ ^^ { x => () => { stack.push(if (stack.pop() * x() == 0) 1 else 0) } }
+              | "KNOCK KNOCK" ~> value /* ^ */ ^^ { x => () => { stack.push(if (stack.pop() * x() != 0) 1 else 0) } })
 
-  def arith = ( ("GET UP" ~> value) /* + */
-              | ("GET DOWN" ~> value) /* - */
-              | ("YOU'RE FIRED" ~> value) /* * */
-              | ("HE HAD TO SPLIT" ~> value)) /* / */
-  def logic = ( ("YOU ARE NOT YOU YOU ARE ME" ~> value) /* == */
-              | ("LET OFF SOME STEAM BENNET" ~> value) /* > */
-              | ("CONSIDER THAT A DIVORCE" ~> value) /* V */
-              | ("KNOCK KNOCK" ~> value)) /* ^ */
-
-
-  def unquotedString = stringLiteral ^^ { s => s.substring(1, s.length - 1) }
-  def value = id | intNumber
-  def id = """[a-zA-Z][a-zA-Z0-9_]*""".r // ^^ { x => symbol_table(x) }
-  def intNumber = """-?[0-9]+""".r ^^ { _.toInt }
+  def value: Parser[() => Int] = intNumber | id
+  def id: Parser[() => Int] = """[a-zA-Z][a-zA-Z0-9_]*""".r ^^ { x => () => symbol_table(x) }
+  def intNumber: Parser[() => Int] = wholeNumber ^^ { x => () => x.toInt }
+  def unquotedString: Parser[() => String] = stringLiteral ^^ { x => () => x.substring(1, x.length - 1) }
 }
 
 object ArnoldCEvaluator {
@@ -40,7 +47,7 @@ object ArnoldCEvaluator {
       val source = Source.fromFile(arg)
       val input = source.mkString
       p.parseAll(p.program, input) match {
-        case p.Success(result, _) => result
+        case p.Success(result, _) => println(result)
         case p.Failure(msg, _) => println("FAILURE: " + msg)
         case p.Error(msg, _) => println("ERROR: " + msg)
       }
