22718f4c682d020732cb8461c9005e01cfab0f6f
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-01-05 08:51:47 +0100

    Add LogLang2
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lab2/ex/log_lang/LogLang2.scala
+++ lab2/ex/log_lang/LogLang2.scala
@@ -8,23 +8,25 @@ class LogLangParser extends JavaTokenParsers {
 
     def program = tasks
     def tasks = rep1(task)
-    def task = "task" ~> ident ~ ("{" ~> statements <~ "}") ^^ { case n ~ s => i = 1; println("Task " + n); s() } 
-
+    def task = "task" ~> ident ~ ("{" ~> statements <~ "}") ^^ { case n ~ s => i = 1; println("Task " + n); s() }
     def statements: Parser[() => Any] = rep(statement) ^^ { case l => () => l.foreach(a => a()) }
     def statement = (remove | rename | backup | merge) ^^ { case x => () => print("  [op" + i + "] "); println(x()); i += 1 }
-
-    def remove: Parser[() => Any] = "remove" ~> file_name ^^ { case x => () => { new File(x).delete() } }
-    def rename: Parser[() => Any] = "rename" ~> file_name ~ file_name ^^ { case x ~ y => () => { new File(x).renameTo(new File(y)) } }
-    def backup: Parser[() => Any] = "backup" ~> file_name ~ file_name ^^ { case x ~ y => () => {
+    def remove: Parser[() => Boolean] = "remove" ~> file_name ^^ { case x => () => { new File(x).delete() } }
+    def rename: Parser[() => Boolean] = "rename" ~> file_name ~ file_name ^^ { case x ~ y => () => { new File(x).renameTo(new File(y)) } }
+    def backup: Parser[() => Boolean] = "backup" ~> file_name ~ file_name ^^ { case x ~ y => () => {
+            try { new FileOutputStream(y).getChannel().transferFrom(new FileInputStream(x).getChannel(), 0, Long.MaxValue); true } 
+            catch { case e: Exception => false }
+        }
+    }
+    def merge: Parser[() => Any] = "merge" ~> file_name ~ file_name ~ file_name ^^ { case x ~ y ~ z => () => {
+            var m = new FileOutputStream(z, true);
             try {
-                new FileInputStream(x).getChannel().transferTo(0, Long.MaxValue, new FileOutputStream(x).getChannel())
-            } catch {
-                false
-            }
-            true
+                m.getChannel().transferTo(0, Long.MaxValue, new FileOutputStream(x).getChannel());
+                m.getChannel().transferTo(0, Long.MaxValue, new FileOutputStream(y).getChannel());
+                true
+            } catch { case e: Exception => false }
         } 
     }
-    def merge: Parser[() => Any] = "merge" ~> file_name ~ file_name ~ file_name ^^ { case x ~ y ~ z => () => () }
 
     def file_name = stringLiteral ^^ { s => s.substring(1, s.length - 1) }
 }
