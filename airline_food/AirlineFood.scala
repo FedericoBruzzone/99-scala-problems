b1d30dcdbe1c593bde7123d2599ef502df4a6d41
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-01-06 12:26:34 +0100

    End AirlineFood (simple)
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- airline_food/AirlineFood.scala
+++ airline_food/AirlineFood.scala
@@ -4,35 +4,39 @@ import scala.util.parsing.combinator._
 import scala.collection.mutable._
 import scala.io._
 
-class AirlineFoodParser(var s: Stack[Int]) extends JavaTokenParsers {
+class AirlineFoodParser(var s: List[Tuple2[String, Int]], var p: Int) extends JavaTokenParsers {
   def program = stmts
 
   def stmts = rep(stmt)
-  def stmt = init | init_and_point | p_down | p_up | set_p | add_to_p_loc | sub_to_p_loc | mul_to_p_loc | jump_to | jump_form | stdin | stdout
-  def init = "You" ~> "ever" ~> "notice" ~> vari <~ "?"
-  def init_and_point = "What's" ~> "the" ~> "deal" ~> "with" ~> vari <~ "?"
-  def p_down = "Um,"
-  def p_up = "Yeah,"
-  def set_p = "Let's" ~> "talk" ~> "about" ~> vari <~ "."
-  def add_to_p_loc = "It's" ~> "kinda" ~> "like" ~> vari <~ "."
-  def sub_to_p_loc = "Not" ~> "like" ~> vari <~ "."
-  def mul_to_p_loc = "Just" ~> "like" ~> vari <~ "."
-  def jump_to = "So..."
-  def jump_form = "Moving on..."
-  def stdin = "Right?"
-  def stdout = "See?"
+  def stmt: Parser[Any] = init | init_and_point | p_down | p_up | set_p | add_to_p_loc | sub_to_p_loc | mul_to_p_loc | stdin | stdout | loop 
+  def init = "You" ~> "ever" ~> "notice" ~> vari <~ "?" ^^ { case v => s = s ::: List((v, 1)); }
+  def init_and_point = "What's" ~> "the" ~> "deal" ~> "with" ~> vari <~ "?" ^^ { case v => s = s ::: List((v, 1)); p = s.length - 1; }
+  def p_down = "Um," ^^ { case _ => if (p > 0) { p -= 1; } }
+  def p_up = "Yeah," ^^ { case _ => if (p < s.length - 1) { p += 1; } }
+  def set_p = "Let's" ~> "talk" ~> "about" ~> vari <~ "." ^^ { case v => p = get_index_of(v); }
+  def add_to_p_loc = "It's" ~> "kinda" ~> "like" ~> vari <~ "." ^^ { case v => s = s.updated(p, (s(p)._1, s(p)._2 + get_value_of(v))) }
+  def sub_to_p_loc = "Not" ~> "like" ~> vari <~ "." ^^ { case v => s = s.updated(p, (s(p)._1, s(p)._2 - get_value_of(v)))}
+  def mul_to_p_loc = "Just" ~> "like" ~> vari <~ "." ^^ { case v => s = s.updated(p, (s(p)._1, s(p)._2 * get_value_of(v)))}
+  def loop = "So..." ~> body <~ "Moving on..." ^^ { case b => while(s(p)._2 != 0) { parseAll(stmts, b) } }
+  def stdin = "Right?" ^^ { case _ => s = s.updated(p, (s(p)._1, StdIn.readInt()))}
+  def stdout = "See?" ^^ { case _ => print(s(p)._2) }
 
-  def vari = """[^\?!\.]+""".r 
+  def body = """(?s).*?(?=Moving on...)""".r ^^ { case b => b } // All until the next Moving on...
+
+  def get_index_of(v: String): Int = { return s.indexWhere(_._1 == v) }
+  def get_value_of(v: String): Int = { return s(get_index_of(v))._2 }
+
+  def vari = """[^\?!\.]+""".r ^^ { case v => v }
 }
 
 object AirlineFoodEvauator {
   def main(args: Array[String]) {
-    val p = new AirlineFoodParser(new Stack[Int]())
+    val p = new AirlineFoodParser(List(), 0)
     args.foreach { arg =>
       val source = scala.io.Source.fromFile(arg)
       val input = source.mkString
       p.parseAll(p.program, input) match {
-        case p.Success(result, _) => println(result)
+        case p.Success(result, _) => result
         case p.Failure(msg, _) => println("FAILURE: " + msg)
         case p.Error(msg, _) => println("ERROR: " + msg)
       }
